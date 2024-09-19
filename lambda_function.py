4f757d485c600c9106f13b8adf1e6542f07bd27d
sergiojulio <sergiojulio@gmail.com>
2024-09-18 22:08:39 -0300

    changes

--- lambda_function.py
+++ lambda_function.py
@@ -124,7 +124,7 @@ def query(schema):
       
 
 #EXTRACT
-def extract(date):
+def extract(env, date):
     try:
 
         # date to timestamp
@@ -212,7 +212,7 @@ def get_tracks(start, end, page):
 
 
 #LOAD
-def load(date, env):
+def load(env, date):
 
     from datetime import date as dt
     from pyiceberg.catalog.sql import SqlCatalog
@@ -255,7 +255,7 @@ def load(date, env):
 
 
 #TRANSFORM 
-def transformation(date):
+def transformation(env, date):
 
     from pyiceberg.catalog.sql import SqlCatalog
     from pyiceberg.expressions import EqualTo, NotEqualTo
@@ -310,6 +310,7 @@ def handler(event, context):
 
     match step:
         case "extract":
+            # 
             return "extract"
         case "load":
             return {
